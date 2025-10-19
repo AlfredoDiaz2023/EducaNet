5c7cbf8c7db4859a1587e92a2fafa583e1ee71ba
AlfredoDiaz2023 <136130223+AlfredoDiaz2023@users.noreply.github.com>
2025-10-18 22:47:01 -0300

    Se integra Firebase y backend de autenticacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -16,6 +19,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -36,6 +42,16 @@ android {
     }
     buildFeatures {
         compose = true
+    }
+
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+
+    packaging {
+        resources{
+            excludes += "/META-INF/{AL2.0, LGPL2.1"
+        }
     }
 }
 
@@ -60,9 +76,10 @@ dependencies {
     //Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebbase-firestore-ktx")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
 
     //Kotlix-Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
--- app/src/main/java/com/example/educanet/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/educanet/ui/screens/login/LoginScreen.kt
@@ -1,125 +1,87 @@
 package com.example.educanet.ui.screens.login
 
 import androidx.compose.runtime.Composable
-
-import android.widget.Toast //Mensaje emergentes
-import androidx.compose.foundation.layout.* //Organizar los elementos en una vista
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.text.KeyboardOptions //Mostrar la entre de datos al usuario
-import androidx.compose.material3.* //Elementos para diseñar UI
-import androidx.compose.runtime.* // Manejar los estados de la app
-import androidx.compose.ui.Alignment // Alinear los elementos
-import androidx.compose.ui.Modifier //Modificar el diseño visual de los elemento
-import androidx.compose.ui.platform.LocalContext //obtener el contexto o estado en ejecución del ciclo de vida de la app y poder mostrar mensaje
-import androidx.compose.ui.text.input.KeyboardType //Controlar el tipo de entrada para el usuario
-import androidx.compose.ui.text.input.PasswordVisualTransformation //Ocultar la contraseña al escribirla
-import androidx.compose.ui.unit.dp //Controlar el tamaño de los elementos
-import androidx.compose.ui.graphics.Color //Controlar el color de los elementos
-
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.educanet.repository.AuthRepository
-import com.example.educanet.viewmodel.LoginViewModel
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import com.example.educanet.model.Usuario
 
 @Composable
-fun LoginScreen(onRegisterClick: () -> Unit = {}, //Agregar al nuevo LoginScreen
-                onLoginSuccess: (user: com.example.educanet.model.Usuario ) -> Unit = {}) {
-    //Variable para obtener en tiempo de ejecución el estado del ciclo de vida de app
+fun LoginScreen(){ // Funcion de inicio de sesion
+    // Variable que permite obtener en tiempo de ejecucion el estado de ciclo de vida app
     val context = LocalContext.current
 
-    //Variable para almacenar en nombre del usuario ** cabiar correo
-    var correo by remember { mutableStateOf("") }
+    // Variable es para almacenar el dato de usuario para el login
+    var usuario by remember { mutableStateOf("") }
 
-    //Variable para almacenar la clave del usuario
+    // Variable es para alamacenar el dato de la password para el login
     var pass by remember { mutableStateOf("") }
 
-    val viewModel: LoginViewModel =viewModel()
-    val user by viewModel.user.collectAsState()
-    val carga by viewModel.carga.collectAsState()
-
-    val repositorio = AuthRepository()//Agregar al nuevo LoginScreen
-
-    //Observar cuando el usuario este logueado
-    LaunchedEffect(user) {
-        user?.let {
-            val mensaje = when (it.rol) {
-                "admin" -> "Bienvenido Admin: ${it.nombre}"
-                else -> "Bienvenido: ${it.nombre}"
-            }
-            Toast.makeText(context, mensaje, Toast.LENGTH_LONG).show()
-            onLoginSuccess(it)
-        }
-    }
-
-    //Configuración para organizar los elementos de la pantalla usando el componente Column()
+    // Componente Column para configurar la organizacion visual de los componentes
     Column (
         modifier = Modifier
-            .fillMaxSize() //Rellenar todo el espacio diponible de la pantalla
+            .fillMaxSize()
             .padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        //Componente tipo Text() para agregar un título
-        Text("Inciar Sesión",
+    ) {
+        // Componente Text para agregar un texto que indique en que vista me encuentro
+        Text("Iniciar Sesion",
             style = MaterialTheme.typography.headlineSmall,
-            color = Color(0xFF4CAF50))
+            color = Color(0xFF59A45B)
+            )
+        // Espacio para alejar el text del input
+        Spacer(Modifier.height(16.dp))
 
-        //Componente Spacer() para agregar un separador entre los elementos
-        Spacer(Modifier.height(46.dp))
-
-        //Componente tipo OutlinedTextField() para ingresar datos por usuario
+        // Componente OutlinedTextField para crear el input del usuario
         OutlinedTextField(
-            //Variable para el nombre del usuario
-            value = correo,
-            onValueChange = { correo = it },
-            label = { Text("Correo", color = Color(0xFFFF5722))},
-            singleLine = true,
-            modifier = Modifier.fillMaxWidth()
+            value = usuario, // Obtener el valor del input y guardarlo en la variable usuario
+            onValueChange = { usuario = it}, // Actualizar la variable usuario con el nuevo ingreso del input
+            label = { Text("Usuario", color = Color(0xFF21C2B2))}, // Agregar titulo Usuario al input
+            singleLine = true, // Permite que el texto del input quede en una sola linea
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            modifier = Modifier.fillMaxWidth() // El input rellena la pantalla segun su ancho
+
         )
 
-        //Componente Spacer() para agregar un separador entre los elementos
+        // Espacio para alejar los input
         Spacer(Modifier.height(10.dp))
 
-        //Componente tipo OutlinedTextField() para ingresar datos por usuario
+        // Componente OutlinedTextField para crear el input de la password
         OutlinedTextField(
-            //Variable para la clave del usuario
-            value = pass,
-            onValueChange = { pass = it },
-            label = { Text("Clave", color = Color(0xFFFF5722))},
-            singleLine = true,
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth()
+            value = pass, // Obtener el valor del input y guardarlo en la variable pass
+            onValueChange = { pass = it}, // Actualizar la variable usuario con el nuevo ingreso del input
+            label = { Text("Contraseña", color = Color(0xFF21C2B2))}, // Agregar titulo Usuario al input
+            singleLine = true, // Permite que el texto del input quede en una sola linea
+            visualTransformation = PasswordVisualTransformation(), // Oculta la cpntraseña al escribirla
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password), // Define un teclado para ingresar el dato al input
+            modifier = Modifier.fillMaxWidth() // El input rellena la pantalla segun su ancho
         )
+        // Espacio para alejar el input del boton
+        Spacer(Modifier.height(14.dp))
 
-        //Componente Spacer() para agregar un separador entre los elementos
-        Spacer(Modifier.height(30.dp))
-
-        //Componente Button() para agrega un boton
+        // Componente Button para agregar un boton a la vista login
         Button(
             onClick = {
-                if (correo.isEmpty() || pass.isEmpty()) {
-                    Toast.makeText(context, "Complete todos los campos", Toast.LENGTH_SHORT).show()
-                    return@Button
-                }
-                viewModel.login(correo, pass)
+                Toast.makeText(context, "Bienvenido $usuario", Toast.LENGTH_SHORT)
+                    .show()
             },
             modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF81154C),
-                contentColor = Color(0xFFC7F9CC)
-            ),
-            enabled = !carga
-        ){
-            if(carga) {
-                CircularProgressIndicator(modifier = Modifier.size(16.dp), color = Color.Gray)
-            } else {
-                Text("Entrar")
-            }
-        }
-        //Botón para agregar registro
-        Spacer(Modifier.height(16.dp))
-        TextButton(onClick = onRegisterClick) {
-            Text("¿No tienes cuenta? Registrate aquí", color = Color(0xFF81154C))
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFA40E31), // Establecer el color de Fondo
+                contentColor = Color(0xFFD7EA1E) // Establece el color de texto
+            )
+        ) {
+            Text("Entrar")
         }
     }
 }
